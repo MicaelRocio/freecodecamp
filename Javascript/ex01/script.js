7bded514b19f53b6dea8ab97eaa3b5d1ad4e4858
MicaelRocio <micaelrocio.ma@gmail.com>
2024-02-13 18:30:32 -0300

    atualização

--- Javascript/ex01/script.js
+++ Javascript/ex01/script.js
@@ -15,11 +15,42 @@ const goldText = document.querySelector("#goldText")
 const monsterStats = document.querySelector("#monsterStats")
 const monsterName = document.querySelector("#monsterName")
 const monsterHealthText = document.querySelector("#monsterHealth")
+const weapons = [
+    {
+        name: "Graveto",
+        power : 5,
+    },
+    {
+        name: "Adaga",
+        power : 30,
+    },
+    {
+        name: "Marelo de garra",
+        power : 50,
+    },
+    {
+        name: "Espada",
+        power : 100,
+    }
+]
 const locations = [
     {
         name: "town square",
         "button text": ["Ir para Loja", "Ir para caverna", "Lutar contra o Dragão"],
-        "button functions": [goStore, goCave, fightDragon]
+        "button functions": [goStore, goCave, fightDragon],
+        text: "Você está na praça da cidade. Você vê uma placa que diz \"Loja\"."
+    },
+    {
+        name: "store",
+        "button text": ["Comprar 10 de vida(10 ouro)", "Comprar arma (30 ouro)", "Ir para praça da cidade"],
+        "button functions": [buyHealth, buyWeapon, goTown],
+        text: "Você entra na loja."
+    },
+    {
+        name: "caverna",
+        "button text": ["Lutar contra Slime", "Lutar contra a Besta de Presas", "Ir para praça da cidade"],
+        "button functions": [fightSlime, fightBeast, goTown],
+        text: "Você entra na caverna. Você vê alguns monstros."
     }
 ]
 
@@ -28,30 +59,25 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    button1.innerText = location["button text"][0];
+    button2.innerText = location["button text"][1];
+    button3.innerText = location["button text"][2];
+    button1.onclick = location["button functions"][0];
+    button2.onclick = location["button functions"][1];
+    button3.onclick = location["button functions"][2];
+    text.innerText = location.text;
 }
 
 function goTown() {
-    button1.innerText = "Ir para Loja";
-    button2.innerText = "Ir para caverna";
-    button3.innerText = "Lutar contra o Dragão";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "Você está na praça da cidade. Você vê uma placa que diz \"Loja\".";
-  }
+    update(locations[0]);
+}
 
 function goStore() {
-    button1.innerText = "Comprar 10 de vida (10 ouro)";
-    button2.innerText = "Comprar arma (30 ouro)";
-    button3.innerText = "Ir para a praça da cidade";
-    button1.onclick = buyHealth;
-    button2.onclick = buyWeapon;
-    button3.onclick = goTown;
-    text.innerText = "Você entra na loja.";
+    update(locations[1]);
 }
 
 function goCave() {
-    console.log("Going to cave.");
+    update(locations[2]);
 }
 
 function fightDragon() {
@@ -59,9 +85,30 @@ function fightDragon() {
 }
 
 function buyHealth() {
-
+    if (gold >= 10) {
+        gold -= 10;
+        health += 10;
+        healthText.innerText = health;
+        goldText.innerText = gold;
+    } else {
+        text.innerText = "Você não tem ouro para comprar vida."
+    }
 }
 
 function buyWeapon() {
+    if (gold >= 30) {
+        gold -= 30;
+        currentWeapon++;
+        goldText.innerText = gold;
+        let newWeapon = weapons;
+        text.innerText = "Agora você tem uma nova arma."
+    }
+}
+
+function fightSlime() {
+    
+}
+
+function fightBeast() {
     
 }
